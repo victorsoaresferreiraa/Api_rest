111dc88b61bc881b044216899a51fe3928870cdd
victor <victorsoaresferreira09@gmail.com>
2024-07-05 13:23:57 -0300

    terminando o projeto api

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# projeto api 
+# Projeto api 
 
 # Rest = Maneira de como vamos fazer, protocologo de arquitetura 
 # Resfull = Aplicabilidade do rest
--- app.py
+++ app.py
@@ -1,32 +1,97 @@
-from flask import Flask, request
-from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from pydantic import BaseModel
+from itertools import count
+from typing import Optional
 
-from tinydb import TinyDB
+from flask import Flask, request, jsonify
+from flask_pydantic_spec import (
+    FlaskPydanticSpec, Response, Request
+)
+from pydantic import BaseModel, Field
+from tinydb import TinyDB, Query
+from tinydb.storages import MemoryStorage
+
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec('flask', title='Live de Python')
 spec.register(server)
-database = 
+database = TinyDB(storage=MemoryStorage)
+c = count()
+
+
+class QueryPessoa(BaseModel):
+    id: Optional[int]
+    nome: Optional[str]
+    idade: Optional[int]
 
 
 class Pessoa(BaseModel):
-    id: int
+    id: Optional[int] = Field(default_factory=lambda: next(c))
     nome: str
-    idade: int 
+    idade: int
 
-@server.get('/')
+
+class Pessoas(BaseModel):
+    pessoas: list[Pessoa]
+    count: int
+
+
+@server.get('/pessoas')  # Rota, endpoint, recurso ...
+@spec.validate(
+    query=QueryPessoa,
+    resp=Response(HTTP_200=Pessoas)
+)
+def buscar_pessoas():
+    """Retorna todas as Pessoas da base de dados."""
+    query = request.context.query.dict(exclude_none=True)
+    todas_as_pessoas = database.search(
+        Query().fragment(query)
+    )
+    return jsonify(
+        Pessoas(
+            pessoas=todas_as_pessoas,
+            count=len(todas_as_pessoas)
+        ).dict()
+    )
+
+@server.get('/pessoas/<int:id>')
 @spec.validate(resp=Response(HTTP_200=Pessoa))
-def pegar_pessoas():
-    return 'Victor soares'
+def buscar_pessoa(id):
+    """Retorna todas as Pessoas da base de dados."""
+    try:
+        pessoa = database.search(Query().id == id)[0]
+    except IndexError:
+        return {'message': 'Pessoa not found!'}, 404
+    return jsonify(pessoa)
 
-server.post('/')
-@spec.validate(body=Pessoa, resp=Pessoa(HTTP_200=Pessoa))
+
+@server.post('/pessoas')
+@spec.validate(
+    body=Request(Pessoa), resp=Response(HTTP_201=Pessoa)
+)
 def inserir_pessoa():
+    """Insere uma Pessoa no banco de dados."""
     body = request.context.body.dict()
+    database.insert(body)
     return body
-    
-    
 
-if __name__ == "__main__":
-    server.run()
+
+@server.put('/pessoas/<int:id>')
+@spec.validate(
+    body=Request(Pessoa), resp=Response(HTTP_201=Pessoa)
+)
+def altera_pessoa(id):
+    """Altera uma Pessoa no banco de dados."""
+    Pessoa = Query()
+    body = request.context.body.dict()
+    database.update(body, Pessoa.id == id)
+    return jsonify(body)
+
+
+@server.delete('/pessoas/<int:id>')
+@spec.validate(resp=Response('HTTP_204'))
+def deleta_pessoa(id):
+    """Remove uma Pessoa do banco de dados."""
+    database.remove(Query().id == id)
+    return jsonify({})
+
+
+server.run()
